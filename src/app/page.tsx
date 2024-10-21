0967c1bd80fd521f6756fe0d5a04d4945660096f
HaiBeZ <994892083@qq.com>
2024-10-22 02:27:58 +0800

    feat: init a wasm demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import { add } from "../../build/release";
 
 export default function Home() {
+  const height =add(100000,10000);
+  console.log("height：",height);
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -21,7 +25,8 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-        </ol>
+          <li>Height:  { height }</li>
+        </ol> 
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
